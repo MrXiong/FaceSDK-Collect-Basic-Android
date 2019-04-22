0fad33fd206d04cce783354477679f689c4514fe
zx <zhangxiong@zhiaotech.com>
2019-04-22 09:21:05 +0800

    Initial commit

---
+++ facesdk/src/main/gen/demo/face/aip/baidu/com/facesdk/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package demo.face.aip.baidu.com.facesdk;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
